2ad524bc739476195723d29ef5bcd14bf4ba37e2
hiroshiuk1991 <hiroshis91@icloud.com>
2019-12-31 15:54:24 +0000

    added dropdown menus

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,3 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+})
 const usersURL = "http://localhost:3000/users"
 
 const canvas = document.querySelector('#GameBoardCanvas')
@@ -44,4 +47,29 @@ for(var y = 0; y < board.length; y++){
             ctx.stroke();
         }
     }
+}
+
+// function startGame() {
+//     const playerSelect = document.querySelector()
+// }
+
+
+function allPlayers() {
+    fetch(usersURL)
+    .then(resp => resp.json())
+    .then(data => eachPlayer(data));
+}
+
+function eachPlayer(player){
+    player.forEach(p => {
+        dropDownMenu(p)
+    })
+}
+
+function dropDownMenu(p) {
+    const dropMenu = document.querySelector('.dropbtn')
+    const option = document.createElement('option')
+    option.innerText = p.name
+
+    dropMenu.append(option)
 }
